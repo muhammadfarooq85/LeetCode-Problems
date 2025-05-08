81fd35c81bf4b1122bd5b804ba139deb365d143a
Muhammad Farooq <famuhammad907@gmail.com>
2025-05-08 21:32:31 +0500

    75. Sort Colors

--- Sort-Colors/solution.js
+++ Sort-Colors/solution.js
@@ -39,4 +39,4 @@ var sortColors = function (nums) {
     }
 };
 console.log(sortColors([2, 0, 2, 1, 1])); // [ 0, 1, 1, 2, 2 ]
-console.log(sortColors([ 0, 2 1])); // [ 0, 1, 1, 2, 2 ]
+console.log(sortColors([0, 2, 1])); // [ 0, 1, 2 ]
