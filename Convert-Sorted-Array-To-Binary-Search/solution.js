c2f71c9ac1b2d323cc1fc280803224d87b930410
Muhammad Farooq <enmuhammadfarooq@gmail.com>
2025-09-19 07:50:28 +0500

    108. Convert Sorted Array to Binary Search Tree

---
+++ Convert-Sorted-Array-To-Binary-Search/solution.js
@@ -0,0 +1,13 @@
+// 108. Convert Sorted Array to Binary Search Tree
+
+// Problem statement
+
+// Given an integer array nums where the elements are sorted in ascending order, convert it to a height-balanced binary search tree.
+
+// Understand the probelem first
+
+// hmare pas ail sorted array of nums hy jis me elements ascending order me sorted hy. hum ne is array ko binary search tree me convert krna hy.
+
+// Solution
+
+// 
